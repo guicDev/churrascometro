88d49a23469681d8c82b9ba74560d0a4bd0cc3d5
Guilherme <reaper14083008@gmail.com>
2024-03-15 01:58:29 -0400

    churrascometro

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Projeto feito para calcular a quantidade de bebida e comida necessária em 
+um churrasco com base no número de pessoas e a duração do churrasco
+Regra:
+Carne - 400gr por pessoa + de 6 horas - 650 gr
+Cerveja - 1200 ml por pesssoa + de 6 horas - 2000 ml
+Refrigerante/água - 1000 ml por pessoa + de 6 horas - 1500 ml
+crianças valem por 0,5.
---
+++ script.js
@@ -0,0 +1,48 @@
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+    console.log("calculando...");
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qntTotalCarne = carnepp(duracao) * adultos + (carnepp(duracao) / 2 * criancas);
+    let qntdTotalCerveja = cervejapp(duracao) * adultos;
+    let qntTotalBebidas = bebidaspp(duracao) * adultos + (bebidaspp(duracao) / 2 * criancas);
+
+    resultado.innerHTML = `<p>${qntTotalCarne/1000} Kg de Carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qntdTotalCerveja/355)} Latas de Cerveja</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qntTotalBebidas/2000)} Garrafas de Bebida</p>`
+}
+
+function carnepp(duracao) {
+    if (duracao >= 6) {
+        return 650
+    }
+    else {
+        return 400
+    }
+}
+
+function cervejapp(duracao) {
+    if (duracao >= 6) {
+        return 2000
+    }
+    else {
+        return 1200
+    }
+}
+
+function bebidaspp(duracao) {
+    if (duracao >= 6) {
+        return 1500
+    }
+    else {
+        return 1000
+    }
+}
